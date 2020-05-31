6f77dfb510f2c755552e65595044d81ec4ecf97d
Matt Fry <Matt Fry>
2020-05-31 11:18:01 -0400

    hitting wrong api link, corrected

--- YahooFinance/Program.cs
+++ YahooFinance/Program.cs
@@ -24,19 +24,39 @@ namespace YahooFinance
             var stockQuote = new StockQuote();
             //var symbol = "DKNG";
             var apiKey = "EXGBU46FGX9CDJ24"; 
-            // retrieve your api key from https://www.alphavantage.co/support/#api-key
+            
             var monthlyPrices = $"https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={symbol}&apikey={apiKey}&datatype=csv".GetStringFromUrl().FromCsv<List<AlphaVantageData>>();
-
+            var dailyPrices = $"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey={apiKey}&datatype=csv".GetStringFromUrl().FromCsv<List<StockQuote>>(); 
+            
             var ticker = symbol;
             var search = new Search { Ticker = ticker };
-
+            var daySearch = new DailySearch { Ticker = ticker };
             int totalRecords = monthlyPrices.Count;
 
             var stockOutput = search.Results = monthlyPrices.OrderByDescending(x => x.Timestamp).Take(monthInt).ToList();
+            
+            
+            var changePercent = daySearch.Results = dailyPrices.Take(1).ToList();
             //monthlyPrices.PrintDump();
-            stockOutput.PrintDump();
+            //stockOutput.PrintDump();
 
-			
+            Console.WriteLine("Trailing Monthly Closing Prices for " + symbol);
+            Console.WriteLine("-------------------");
+
+            foreach (var m in search.Results)
+            {
+                Console.WriteLine("Timestamp:  " + m.Timestamp);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Close:  " + m.Close);
+                Console.ResetColor();
+                Console.WriteLine("-------------------");
+            }
+
+            foreach (var x in daySearch.Results)
+            {
+                Console.WriteLine("Change Perfecntage" + x.changePercent);
+            }
+                    
 			//Console.WriteLine("The current price for " + symbol + " is: " + price);
 			//var maxPrice = monthlyPrices.Max(u => u.Close);
 			//var minPrice = monthlyPrices.Min(u => u.Close);
--- YahooFinance/Search.cs
+++ YahooFinance/Search.cs
@@ -19,4 +19,16 @@ namespace YahooFinance
         public string ErrorMessage { get; set; } = "";
     }
 
+    public class DailySearch
+    {
+        public string Ticker { get; set; } = "";
+        public List<StockQuote> Results { get; set; } = new List<StockQuote>();
+
+        public int PageNr { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
+
+        public int TotalRecords { get; set; } = 0;
+        public int TotalPages { get; set; } = 0;
+    }
 }
